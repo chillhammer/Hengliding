56d300de1e0799dc364ff6522b4d319757f5f08f
Cinnamon18 <alice.ava.moh@gmail.com>
2019-02-09 00:38:14 -0500

    basic feeder animation!

--- Assets/Scripts/Raising/Interaction/Feeder.cs
+++ Assets/Scripts/Raising/Interaction/Feeder.cs
@@ -4,5 +4,26 @@ namespace Raising.Interaction {
 
 	public class Feeder : MonoBehaviour {
 
+		public Animator feederAnimator;
+
+		void OnMouseEnter() {
+			feederAnimator.SetTrigger("Open");
+		}
+
+		void OnMouseExit() {
+			feederAnimator.SetTrigger("Close");
+
+		}
+
+		void OnMouseDown() {
+		}
+
+		void OnMouseDrag() {
+			//move food to mouse 
+		}
+
+		void OnMouseUp() {
+			//drop the food
+		}
 	}
 }
