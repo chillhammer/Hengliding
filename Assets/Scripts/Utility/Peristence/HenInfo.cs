f77b6214b0ff168cf3f3af19803dcf79227a39c2
Grace Rarer <gracerarer@gmail.com>
2019-02-19 19:04:14 -0500

    Stats and persistence
    Created the 4 stat fields for hens (love, size, fitness, feather quality). Created a system to save the attributes of each hen (stored as a JSON structure within playerPrefs), and a spawning system that spawns hens at the start of the Raise scene based on the saved data (or spawns 4 default hens if there is no save).

---
+++ Assets/Scripts/Utility/Peristence/HenInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Raising;
+
+
+//serializable container for persistent hen fields
+[System.Serializable]
+public class HenInfo
+{
+    public float sizeStat;
+    public float loveStat;
+    public float fitnessStat;
+    public float feathersStat;
+    public int breedNumber; // we have to cast this to int for the serialization to work properly
+
+    public HenInfo(HenBreed breed, float size, float love, float fitness, float feathers) {
+        this.breedNumber = (int) breed;
+        this.sizeStat = size;
+        this.loveStat = love;
+        this.fitnessStat = fitness;
+        this.feathersStat = feathers;
+    }
+
+    public HenInfo(Hen hen) {
+        this.breedNumber = (int) hen.breed;
+        //TODO use correct stats
+        this.sizeStat = hen.size.value;
+        this.loveStat = hen.love.value;
+        this.fitnessStat = hen.fitness.value;
+        this.feathersStat = hen.featherQuality.value;
+    }
+    
+
+  
+}
